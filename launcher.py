30ccdb3408f71ab2c6b40132559be1280ebe2ee4
Denis Dudnik <Denis.Dudnik@gmail.com>
2022-04-30 16:30:58 +0300

    feat: add launcher

---
+++ launcher.py
@@ -0,0 +1,21 @@
+import subprocess
+
+process_lst = []
+
+while True:
+    action = input(
+        'Choose: q - quit, x - close all windows, s - run server and clients: ')
+
+    if action == 'q':
+        break
+    elif action == 'x':
+        for process in process_lst:
+            process.kill()
+        process_lst = []
+    elif action == 's':
+        process_lst.append(subprocess.Popen(
+            'python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
+
+        for _ in range(5):
+            process_lst.append(subprocess.Popen(
+                'python client.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
